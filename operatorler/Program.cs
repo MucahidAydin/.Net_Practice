6c6e99781c46d2ba12e686374b5178bf522a3496
Mucahid Aydin <mucahidaydin047@gmail.com>
2022-02-07 23:14:55 +0300

    operatorler

---
+++ operatorler/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace operatorler 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                
+        
+
+           bool isSuccess = true;
+           bool isCompelted = false;
+
+           if (isSuccess && isCompelted )
+           Console.WriteLine("Wonderfull!");
+
+           if (isSuccess || isCompelted )
+           Console.WriteLine("Excellent!");
+
+           if (isSuccess && !isCompelted )
+           Console.WriteLine("Good!");
+
+
+         
+
+           int c =99;
+           int d =6;
+           bool sonuc = c<d;           
+           Console.WriteLine(sonuc);
+           sonuc = d<c;
+           Console.WriteLine(sonuc);
+           sonuc = d==c;
+           Console.WriteLine(sonuc);
+           sonuc = d>=c;
+           Console.WriteLine(sonuc);
+           sonuc = d<=c;
+
+        
+
+           int g =46;
+           int h =50;
+           int sonuc1 = g + h;
+           Console.WriteLine(sonuc1);
+           sonuc1 = g + h;
+           Console.WriteLine(sonuc1);
+           sonuc1 = g * h;
+           Console.WriteLine(sonuc1);
+           sonuc1 = g / h;
+           Console.WriteLine(sonuc1);
+           sonuc1 = h++;
+           Console.WriteLine(sonuc1);
+
+      
+
+           int kalan = 20%3;
+           Console.WriteLine(kalan);
+        }
+    }
+}
