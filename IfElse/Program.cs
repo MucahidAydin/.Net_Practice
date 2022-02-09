bd58deb94c3295768da7b3f2ebf1ddb56d7fa47e
Mucahid Aydin <mucahidaydin047@gmail.com>
2022-02-09 17:26:36 +0300

    IfElse

---
+++ IfElse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IfElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time >=6 && time<11)
+                Console.WriteLine("günaydın!");
+            else if(time<= 18)
+                Console.WriteLine("iyi günler!");
+            else
+                Console.WriteLine("iyi geceler!");
+
+            string sonuc = time<=18 ? "iyi günler" :"iyi geceler";//Ternary
+
+            Console.WriteLine(sonuc);
+
+            sonuc = time>=6 && time<11 ?"günaydın" : time<=18 ? "iyi günler" : "iyi geceler";
+
+            Console.WriteLine(sonuc);
+
+                 
+
+            Console.ReadKey();
+        }
+    }
+}
