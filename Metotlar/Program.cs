08a89aac498cde5a9722ced54d5208c14906b08f
Mucahid Aydin <mucahidaydin047@gmail.com>
2022-02-14 23:36:18 +0300

    Metotlar

---
+++ Metotlar/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Metotlar
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int a=4,b=3;
+            metotlar ornek=new metotlar();
+            ornek.EkranaYazdır(toplam(a,b));
+            
+            ornek.ArtırVeTopla(ref a,ref b);
+            ornek.EkranaYazdır(toplam(a,b));
+            
+            Console.ReadKey();
+
+            
+		}
+        static int toplam(int deger1,int deger2)
+        {
+            return deger1+deger2;
+        }
+
+	}
+    class metotlar
+    {
+        public void EkranaYazdır(int veri)
+        {
+            Console.WriteLine(Convert.ToString(veri));
+        }
+
+        public int ArtırVeTopla(ref int deger1,ref int deger2)
+        {
+            deger1++;
+            deger2+=10;
+            return deger1+deger2;
+        }
+    }
+}
