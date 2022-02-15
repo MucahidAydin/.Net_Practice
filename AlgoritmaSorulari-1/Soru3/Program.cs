b8b3920f7f8f60c761fe97205e55afe9f3489d35
Mucahid Aydin <mucahidaydin047@gmail.com>
2022-02-15 21:06:34 +0300

    AlgoritmaSoru3

---
+++ AlgoritmaSorulari-1/Soru3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Soru3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" n sayısını giriniz: ");
+		    int sayi=Convert.ToInt32(Console.ReadLine());		   
+
+            string[] dizi=new string[sayi];
+		    for (int i = 0; i < sayi; i++)
+			{
+				Console.Write("{0}. kelimeyi giriniz : ",(i+1));
+				string kelime = Console.ReadLine();
+                dizi[i]= kelime;
+			}
+            for (int i = (sayi-1); i >= 0; i--)
+            {
+                Console.WriteLine(dizi[i]);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
