69b83a20fc0dcb7d50ad249e9c27ebf5a0df9263
Mücahid AYDIN <67193955+MucahidAydin@users.noreply.github.com>
2022-02-15 21:08:51 +0300

    AlgoritmaSoru3

--- AlgoritmaSorulari-1/Soru3/Program.cs
+++ AlgoritmaSorulari-1/Soru3/Program.cs
@@ -7,15 +7,15 @@ namespace Soru3
         static void Main(string[] args)
         {
             Console.Write(" n sayısını giriniz: ");
-		    int sayi=Convert.ToInt32(Console.ReadLine());		   
+	    int sayi=Convert.ToInt32(Console.ReadLine());		   
 
             string[] dizi=new string[sayi];
-		    for (int i = 0; i < sayi; i++)
-			{
-				Console.Write("{0}. kelimeyi giriniz : ",(i+1));
-				string kelime = Console.ReadLine();
-                dizi[i]= kelime;
-			}
+	    for (int i = 0; i < sayi; i++)
+	    {
+		Console.Write("{0}. kelimeyi giriniz : ",(i+1));
+		string kelime = Console.ReadLine();
+           	dizi[i]= kelime;
+	    }
             for (int i = (sayi-1); i >= 0; i--)
             {
                 Console.WriteLine(dizi[i]);
