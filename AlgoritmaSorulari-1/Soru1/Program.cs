20b04931a0c9ef589b6c2be8104c6671f3098172
Mucahid Aydin <mucahidaydin047@gmail.com>
2022-02-15 19:04:07 +0300

    AlgoritmaSoru1

---
+++ AlgoritmaSorulari-1/Soru1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Soru1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Dizi boyutunu giriniz : ");
+			int sayi=Convert.ToInt32(Console.ReadLine());
+
+			for (int i = 0; i < sayi; i++)
+			{
+				Console.Write("{0}. sayıyı giriniz : ",(i+1));
+				int sayi2=Convert.ToInt32(Console.ReadLine());
+				if (sayi2%2==0)
+				{
+					Console.WriteLine("{0} çift",sayi2);
+				}
+				else
+				{
+					Console.WriteLine("{0} tek",sayi2);
+				}
+
+			}
+			
+
+            Console.ReadKey();
+		}
+	}
+}
