39d3760b65d0f1db6698bdec7766186b3297f011
Mucahid Aydin <mucahidaydin047@gmail.com>
2022-02-15 21:56:40 +0300

    AlgoritmaSoru4

---
+++ AlgoritmaSorulari-1/Soru4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Soru4
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+            Console.Write("Bir cümle yazınız : ");
+            string cumle=Console.ReadLine();
+
+            string[] dizi = cumle.Split(" ");        
+            Console.WriteLine("Cümle sayısı : "+dizi.Length);
+            Console.WriteLine("Kelime sayısı : "+string.Join("",dizi).Length);          
+        
+            Console.ReadKey();
+		}
+	}
+}
