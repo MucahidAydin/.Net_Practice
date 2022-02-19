b04e27c16f3f44eb5fce223067c4138c5e65e203
Mucahid Aydin <mucahidaydin047@gmail.com>
2022-02-19 15:32:10 +0300

    AlgoritmaSoru3

---
+++ AlgoritmaSorulari-2/Soru3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace Soru3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir cümle giriniz");
+            string cumle = Console.ReadLine();
+
+            cumle = cumle.ToLower();
+            ArrayList liste = new ArrayList();
+
+            for (int i = 0; i < cumle.Length; i++)
+            {
+                if (cumle[i] == 'i' || cumle[i] == 'ı' || cumle[i] == 'u' || cumle[i] == 'ü' || cumle[i] == 'a' || cumle[i] == 'e' || cumle[i] == 'o' || cumle[i] == 'ö')
+                {
+                    liste.Add(cumle[i]);
+                }
+            }
+
+            liste.Sort();
+            Console.WriteLine("");
+            Console.Write("=> ");
+            foreach (var item in liste)
+                Console.Write(item + " ");
+
+            Console.ReadKey();
+        }
+    }
+}
