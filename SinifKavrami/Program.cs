c3898a0340ce3f4d01ddf3e9b1d134e72a80c021
Mucahid Aydin <mucahidaydin047@gmail.com>
2022-02-21 15:42:31 +0300

    SinifKavrami

---
+++ SinifKavrami/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SinifKavrami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan1 = new Calisan();
+
+            calisan1.Ad = "mücahid";
+            calisan1.Soyad = "aydin";
+            calisan1.No = 00101111;
+            calisan1.Departman = "İnsan Kaynakları";
+            calisan1.CalisanBilgileri();
+
+            calisan1.Ad = "deniz";
+            calisan1.Soyad = "arda";
+            calisan1.No = 10101111;
+            calisan1.Departman = "Satın Alma";
+            calisan1.CalisanBilgileri();
+
+            Console.ReadKey();
+        }
+    }
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Çalışan Adı : {0}", Ad);
+            Console.WriteLine("Çalışan Soyadı : {0}", Soyad);
+            Console.WriteLine("Çalışan Numarası : {0}", No);
+            Console.WriteLine("Çalışan Departmanı : {0}", Departman);
+        }
+    }
+}
