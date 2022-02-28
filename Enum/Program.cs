8ba6fa6c1bcc469240b5ac6da50234ea8d628b60
Mucahid Aydin <mucahidaydin047@gmail.com>
2022-02-28 23:42:58 +0300

    enumeration

---
+++ Enum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Salı);
+            Console.WriteLine((int)Gunler.Pazar);
+
+            int sicaklik = 24;
+
+            if (sicaklik <= (int)HavaDurumu.Normal)
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim.");
+            else if (sicaklik >= (int)HavaDurumu.Sicak)
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün");
+            else if (sicaklik >= (int)HavaDurumu.Normal && sicaklik <= (int)HavaDurumu.Sicak)
+                Console.WriteLine("Hadi dışarıya çıkalım!");
+
+            Console.ReadKey();
+        }
+    }
+    enum Gunler
+    {
+        Pazartesi = 1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi = 35,
+        Pazar
+    }
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sicak = 25,
+        CokSicak = 35
+    }
+}
